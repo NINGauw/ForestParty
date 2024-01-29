755f1beadb5f3b14af5526bebcecb279414b757a
NINGauw <haonam.vt01@gmail.com>
2024-01-30 05:53:51 +0700

    Add Network System
    Add network system, still have movement bug

--- ForestParty/Assets/Script/PlayerMovement.cs
+++ ForestParty/Assets/Script/PlayerMovement.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovement : NetworkBehaviour
 {
     [SerializeField] private AudioSource sound;
     public float speed;
@@ -21,6 +22,10 @@ public class PlayerMovement : MonoBehaviour
     
     private void Update()
     {
+        if(!IsOwner)
+        {
+            return;
+        }
         dirX = Input.GetAxisRaw("Horizontal");
         dirY = Input.GetAxisRaw("Vertical");
         player.velocity = new Vector2(dirX * speed, dirY * speed);
